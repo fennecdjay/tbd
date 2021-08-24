996aad5fd7aadb9a743922cbb8869d1f463ed0bc
Edward Bruce <cometodukey@protonmail.com>
2021-08-24 20:25:46 +0100

    Add fast path for string comparisons.

--- src/lib/string.c
+++ src/lib/string.c
@@ -5,10 +5,10 @@
 
 bool str_equal(const char *s1, const char *s2)
 {
-    return !strcmp(s1, s2);
+    return s1 == s2 || !strcmp(s1, s2);
 }
 
 bool strn_equal(const char *s1, const char *s2, const size_t size)
 {
-    return !strncmp(s1, s2, size);
+    return s1 == s2 || !strncmp(s1, s2, size);
 }
